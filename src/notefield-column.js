300ec6e1eec6b0124d30801a699ef8b8c90c9d7d
Kingdaro <kingdaro@gmail.com>
2016-10-22 13:01:11 -0400

    split off notefield column into module

---
+++ src/notefield-column.js
@@ -0,0 +1,120 @@
+import * as canvas from './canvas'
+import { tail, lerp } from './util'
+import { JudgeLevels, getJudgement, isMissed } from './judgement'
+
+export const columnWidth = 50
+
+const keyHeight = 100
+const keyBorderWidth = 5
+
+const receptorHeight = 24
+
+const noteHeight = receptorHeight
+const noteSpacing = 100 // pixels per second
+
+export function NotefieldColumn (color, columnPosition, scrollSpeed) {
+  // private
+  let pressed = false
+  let brightness = 0
+  let notes = []
+
+  function drawReceptor () {
+    const opacity = lerp(0.3, 0.6, brightness)
+    canvas.setFillColor(color.opacity(opacity))
+    canvas.fillRect(0, canvas.height - keyHeight, columnWidth, -receptorHeight)
+  }
+
+  function drawBacklight () {
+    const opacity = lerp(0.03, 0.15, brightness)
+    canvas.setFillColor(color.opacity(opacity))
+    canvas.fillRect(0, 0, columnWidth, canvas.height)
+  }
+
+  function drawNote ({ time, column }, songTime) {
+    const y = canvas.height - keyHeight - (time - songTime) * noteSpacing * scrollSpeed
+    canvas.setFillColor(color)
+    canvas.fillRect(0, y, columnWidth, -noteHeight)
+  }
+
+  function drawKey () {
+    const dim = lerp(0.3, 0, brightness)
+
+    canvas.batch(() => {
+      canvas.translate(0, canvas.height)
+
+      canvas.setFillColor(color.darken(dim))
+      canvas.fillRect(0, 0, columnWidth, -keyHeight)
+
+      canvas.setStroke(color.darken(dim + 0.2), keyBorderWidth)
+      canvas.translate(keyBorderWidth / 2, -keyBorderWidth / 2)
+      canvas.strokeRect(0, 0, columnWidth - keyBorderWidth, -keyHeight + keyBorderWidth)
+    })
+  }
+
+  // public
+  const receptorPosition = {
+    x: columnPosition + columnWidth / 2,
+    y: canvas.height - keyHeight
+  }
+
+  function addNote (time, column) {
+    notes.push({ time, column })
+  }
+
+  function checkTap (songTime) {
+    const currentNote = tail(notes)
+    if (currentNote) {
+      const timing = songTime - currentNote.time
+      const level = getJudgement(timing)
+      if (level !== JudgeLevels.break) {
+        notes.pop()
+      }
+      return level
+    }
+  }
+
+  function checkMiss (songTime) {
+    const currentNote = tail(notes)
+    if (currentNote && isMissed(songTime - currentNote.time)) {
+      notes.pop()
+      return true
+    }
+  }
+
+  function press () {
+    pressed = true
+  }
+
+  function lift () {
+    pressed = false
+  }
+
+  function updateBrightness (elapsed) {
+    if (pressed) {
+      brightness = 1
+    } else {
+      brightness = lerp(brightness, 0, elapsed * 20)
+    }
+  }
+
+  function draw (songTime) {
+    canvas.batch(() => {
+      canvas.translate(columnPosition, 0)
+      drawBacklight()
+      drawReceptor()
+      notes.forEach(note => drawNote(note, songTime))
+      drawKey()
+    })
+  }
+
+  return {
+    addNote,
+    receptorPosition,
+    checkTap,
+    checkMiss,
+    updateBrightness,
+    draw,
+    press,
+    lift
+  }
+}
--- src/notefield.js
+++ src/notefield.js
@@ -1,23 +1,15 @@
 import { NoteExplosion } from './note-explosion'
-import { Judgement, JudgeLevels, getJudgement, isMissed } from './judgement'
+import { Judgement, JudgeLevels } from './judgement'
+import { NotefieldColumn, columnWidth } from './notefield-column'
 import { White, Black, Gold, Cloudy, Violet } from './color'
-import { lerp, range, tail } from './util'
 import * as canvas from './canvas'
 
 const columnCount = 6
-const columnWidth = 50
 const position = 220
 const borderWidth = 4
-const dividerWidth = 2
-const keyHeight = 100
-const keyBorderWidth = 5
-const receptorHeight = 24
-const noteHeight = receptorHeight
-const noteSpacing = 100 // pixels per second
 
 const backgroundColor = Black.opacity(0.9)
 const borderColor = White.opacity(0.8)
-const dividerColor = White.opacity(0.1)
 
 const columnColors = [ Gold, Cloudy, Violet, Cloudy, Violet, Cloudy ]
 
@@ -27,72 +19,50 @@ export function Notefield (song, config) {
   const explosion = NoteExplosion()
   const judgement = Judgement()
 
-  const columns = range(0, columnCount - 1).map(n => {
-    return {
-      pressed: false,
-      brightness: 0,
-      color: columnColors[n],
-      notes: []
-    }
-  })
-
-  for (const note of song.notes) {
-    const { time, column } = note
-    columns[column].notes.push({ time, column })
-  }
+  const columns = []
 
   let songTime = -2
 
-  function getReceptorPosition (columnIndex) {
-    return [ (columnIndex + 0.5) * columnWidth, canvas.height - keyHeight ]
+  for (let i = 0; i < columnCount; i++) {
+    columns.push(NotefieldColumn(columnColors[i], i * columnWidth, scrollSpeed))
+  }
+
+  for (const note of song.notes) {
+    const { time, column } = note
+    columns[column].addNote(time, column)
   }
 
   function checkTap (column) {
-    const currentNote = tail(column.notes)
-    if (currentNote) {
-      const timing = songTime - currentNote.time
-      const level = getJudgement(timing)
-      if (level !== JudgeLevels.break) {
-        column.notes.pop()
-        judgement.trigger(level)
-        return true
-      }
+    const level = column.checkTap(songTime)
+    if (level && level !== JudgeLevels.break) {
+      const { x, y } = column.receptorPosition
+      explosion.trigger(x, y)
+      judgement.trigger(level)
     }
   }
 
   function checkMiss (column) {
-    const currentNote = tail(column.notes)
-    if (currentNote && isMissed(songTime - currentNote.time)) {
-      column.notes.pop()
+    if (column.checkMiss(songTime)) {
       judgement.trigger(JudgeLevels.break)
-    }
-  }
-
-  function updateColumnBrightness (column, elapsed) {
-    if (column.pressed) {
-      column.brightness = 1
-    } else {
-      column.brightness = lerp(column.brightness, 0, elapsed * 20)
     }
   }
 
   function press (columnIndex) {
     const column = columns[columnIndex]
-    if (checkTap(column)) {
-      explosion.trigger(...getReceptorPosition(columnIndex))
-    }
-    column.pressed = true
+    column.press()
+    checkTap(column)
   }
 
   function lift (columnIndex) {
-    columns[columnIndex].pressed = false
+    const column = columns[columnIndex]
+    column.lift()
   }
 
   function update (elapsed) {
     songTime += elapsed
 
     columns.forEach(column => {
-      updateColumnBrightness(column, elapsed)
+      column.updateBrightness(elapsed)
       checkMiss(column)
     })
 
@@ -113,14 +83,6 @@ export function Notefield (song, config) {
         canvas.fillRect(0, 0, fieldWidth, canvas.height)
       })
 
-      // column dividers
-      // canvas.batch(() => {
-      //   canvas.setFillColor(dividerColor)
-      //   for (let i = 1; i < columnCount; i++) {
-      //     drawColumnDivider(i)
-      //   }
-      // })
-
       // notefield edges
       canvas.batch(() => {
         canvas.setFillColor(borderColor)
@@ -130,30 +92,8 @@ export function Notefield (song, config) {
         canvas.fillRect(0, 0, borderWidth, canvas.height)
       })
 
-      // backlights
-      canvas.batch(() => {
-        columns.forEach(drawBacklight)
-      })
-
-      // receptors
-      canvas.batch(() => {
-        canvas.translate(0, canvas.height - keyHeight - receptorHeight)
-        columns.forEach(drawReceptor)
-      })
-
-      // notes
-      canvas.batch(() => {
-        canvas.translate(0, canvas.height - keyHeight - receptorHeight)
-        for (const col of columns) {
-          col.notes.map(drawNote)
-        }
-      })
-
-      // keys
-      canvas.batch(() => {
-        canvas.translate(0, canvas.height - keyHeight)
-        columns.forEach(drawKey)
-      })
+      // columns
+      columns.forEach(col => col.draw(songTime))
 
       // note explosions
       explosion.draw()
@@ -164,40 +104,6 @@ export function Notefield (song, config) {
         judgement.draw()
       })
     })
-  }
-
-  function drawColumnDivider (col) {
-    const x = columnWidth * col - dividerWidth / 2
-    canvas.fillRect(x, 0, dividerWidth, canvas.height)
-  }
-
-  function drawReceptor ({ color, brightness }, index) {
-    const opacity = lerp(0.3, 0.6, brightness)
-    canvas.setFillColor(color.opacity(opacity))
-    canvas.fillRect(index * columnWidth, 0, columnWidth, receptorHeight)
-  }
-
-  function drawBacklight ({ color, brightness }, index) {
-    const opacity = lerp(0.03, 0.15, brightness)
-    canvas.setFillColor(color.opacity(opacity))
-    canvas.fillRect(index * columnWidth, 0, columnWidth, canvas.height)
-  }
-
-  function drawNote ({ time, column }) {
-    const x = column * columnWidth
-    const y = (time - songTime) * noteSpacing * scrollSpeed
-    canvas.setFillColor(columns[column].color)
-    canvas.fillRect(x, -y, columnWidth, noteHeight)
-  }
-
-  function drawKey ({ color, brightness }, index) {
-    const dim = lerp(0.3, 0, brightness)
-    canvas.setFillColor(color.darken(dim))
-    canvas.fillRect(columnWidth * index, 0, columnWidth, keyHeight)
-
-    canvas.setStroke(color.darken(dim + 0.2), keyBorderWidth)
-    canvas.strokeRect(columnWidth * index + keyBorderWidth / 2, keyBorderWidth / 2,
-      columnWidth - keyBorderWidth, keyHeight - keyBorderWidth)
   }
 
   return { update, draw, press, lift }
