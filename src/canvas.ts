b3cad1e1bf3448a596ba5789ce3e933523f3b277
Kingdaro <kingdaro@gmail.com>
2016-10-23 10:01:33 -0400

    annotate types for canvas API

--- src/canvas.ts
+++ src/canvas.ts
@@ -1,4 +1,5 @@
 import {degrees} from './util'
+import Color from './color'
 
 const { cos, sin, PI } = Math
 const TAU = PI * 2
@@ -10,7 +11,7 @@ export const width = element.width
 export const height = element.height
 
 // drawing
-export function circle (x, y, radius, sides = 0) {
+export function circle (x: number, y: number, radius: number, sides: number = 0) {
   ctx.beginPath()
   if (sides > 0) {
     for (let i = 0; i < sides; i++) {
@@ -25,63 +26,63 @@ export function circle (x, y, radius, sides = 0) {
   ctx.fill()
 }
 
-export function fillRect (x, y, width, height) {
+export function fillRect (x: number, y: number, width: number, height: number) {
   ctx.fillRect(x, y, width, height)
 }
 
-export function strokeRect (x, y, width, height) {
+export function strokeRect (x: number, y: number, width: number, height: number) {
   ctx.strokeRect(x, y, width, height)
 }
 
-export function fillText (text, x, y) {
+export function fillText (text: string, x: number, y: number) {
   ctx.fillText(text, x, y)
 }
 
-export function clear (color) {
+export function clear (color: Color) {
   ctx.fillStyle = color.toString()
   ctx.fillRect(0, 0, width, height)
 }
 
 // graphics state
-export function setFillColor (color) {
+export function setFillColor (color: Color) {
   ctx.fillStyle = color.toString()
 }
 
-export function setStroke (color, width) {
+export function setStroke (color: Color, width: number) {
   ctx.strokeStyle = color.toString()
   ctx.lineWidth = width
 }
 
-export function setStrokeColor (color) {
+export function setStrokeColor (color: Color) {
   ctx.strokeStyle = color.toString()
 }
 
-export function setShadow (ox, oy, blur, color) {
+export function setShadow (ox: number, oy: number, blur: number, color: Color) {
   ctx.shadowOffsetX = ox
   ctx.shadowOffsetY = oy
   ctx.shadowBlur = blur
   ctx.shadowColor = color.toString()
 }
 
-export function setFont (font) {
+export function setFont (font: string) {
   ctx.font = font
 }
 
-export function setTextAlign (align) {
+export function setTextAlign (align: string) {
   ctx.textAlign = align
 }
 
 // transformation
-export function batch (drawops, ...args) {
+export function batch (drawops: Function, ...args) {
   ctx.save()
   drawops(...args)
   ctx.restore()
 }
 
-export function translate (x, y) {
+export function translate (x: number, y: number) {
   ctx.translate(x, y)
 }
 
-export function rotate (deg) {
+export function rotate (deg: number) {
   ctx.rotate(degrees(deg))
 }
