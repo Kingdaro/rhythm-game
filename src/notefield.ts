097252b2648a193d726f769bb7a1cf62ea85c94b
Kingdaro <kingdaro@gmail.com>
2016-10-23 10:04:38 -0400

    temporarily shut up tslint

--- src/notefield.ts
+++ src/notefield.ts
@@ -2,7 +2,8 @@ import * as canvas from './canvas'
 import NoteExplosion from './note-explosion'
 import JudgementAnimation from './judgement'
 // import Clock from './clock'
-import {White, Black, Gold, Cloudy, Violet} from './color'
+// import {White, Black, Gold, Cloudy, Violet} from './color'
+import {White, Black} from './color'
 
 // import { NotefieldColumn, columnWidth } from './notefield-column'
 
@@ -14,7 +15,7 @@ const borderWidth = 4
 const backgroundColor = Black.opacity(0.9)
 const borderColor = White.opacity(0.8)
 
-const columnColors = [Gold, Cloudy, Violet, Cloudy, Violet, Cloudy]
+// const columnColors = [Gold, Cloudy, Violet, Cloudy, Violet, Cloudy]
 
 interface Song {}
 
@@ -27,7 +28,7 @@ class Notefield {
 
   constructor (private song: Song, private config: NotefieldConfig) {}
 
-  update (elapsed: number) {}
+  // update (elapsed: number) {}
 
   draw () {
     const fieldWidth = columnCount * columnWidth
