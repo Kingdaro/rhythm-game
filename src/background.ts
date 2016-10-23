767048461553d3c9922b5b584489f9cf4774e95a
Kingdaro <kingdaro@gmail.com>
2016-10-23 09:04:58 -0400

    sort shapes furthest to closest for parallax

--- src/background.ts
+++ src/background.ts
@@ -1,20 +1,23 @@
 import * as canvas from './canvas'
-import { Red, Orange, Gold, Green, Blue, Violet } from './color'
+import {Red, Orange, Gold, Green, Blue, Violet} from './color'
 import Clock from './clock'
+import {lerp, randomItem} from './util'
 
 const colors = [ Red, Orange, Gold, Green, Blue, Violet ]
 
 class Shape {
   x = Math.random() * canvas.width
   y = canvas.height + 100
-  size = Math.random() * 2 + 2
-  color = colors[ Math.floor(Math.random() * colors.length) ]
+  size = lerp(2, 5, Math.random())
+  color = randomItem(colors)
 
   draw () {
-    canvas.translate(this.x, this.y)
-    canvas.rotate(-90)
-    canvas.setFillColor(this.color)
-    canvas.circle(0, 0, this.size * 20, 3)
+    canvas.batch(() => {
+      canvas.translate(this.x, this.y)
+      canvas.rotate(-90)
+      canvas.setFillColor(this.color)
+      canvas.circle(0, 0, this.size * 20, 3)
+    })
   }
 }
 
@@ -23,10 +26,11 @@ class Background {
   clock = new Clock(0.15)
 
   update (elapsed: number) {
-    if (elapsed > 1) { return }
+    if (elapsed > 1) return
 
     if (this.clock.update(elapsed)) {
       this.shapes.push(new Shape())
+      this.shapes.sort((a, b) => a.size - b.size)
     }
 
     this.shapes.forEach(shape => shape.y -= shape.size * 40 * elapsed)
