0001a075f1d01bc92eeea1638e32b571eea93016
Kingdaro <kingdaro@gmail.com>
2016-10-25 09:52:38 -0400

    add comments

--- src/util.ts
+++ src/util.ts
@@ -1,21 +1,21 @@
 const {random, floor, PI} = Math
 
+// clamp a number between a minimum and maximum
 export function clamp (n: number, min: number, max: number): number {
   return n > max ? max : n < min ? min : n
 }
 
+// interpolate between a and b given a delta 0 to 1
 export function lerp (a: number, b: number, delta: number): number {
   return a + (b - a) * clamp(delta, 0, 1)
 }
 
+// return a random item from a list
 export function randomItem<T> (list: T[]): T {
   return list[floor(random() * list.length)]
 }
 
-export function tail<T> (list: T[]): T {
-  return list[list.length - 1]
-}
-
+// convert a number from degrees to radians
 export function degrees (deg: number): number {
   return deg / 180 * PI
 }
