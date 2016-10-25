1f4a98cb79b545e46f6eaf41dc01d5bc0cf1d217
Kingdaro <kingdaro@gmail.com>
2016-10-25 09:51:20 -0400

    annotate util functions

--- src/util.ts
+++ src/util.ts
@@ -1,11 +1,21 @@
 const {random, floor, PI} = Math
 
-export const clamp = (n, min, max) => n > max ? max : n < min ? min : n
+export function clamp (n: number, min: number, max: number): number {
+  return n > max ? max : n < min ? min : n
+}
 
-export const lerp = (a, b, delta) => a + (b - a) * clamp(delta, 0, 1)
+export function lerp (a: number, b: number, delta: number): number {
+  return a + (b - a) * clamp(delta, 0, 1)
+}
 
-export const randomItem = list => list[floor(random() * list.length)]
+export function randomItem<T> (list: T[]): T {
+  return list[floor(random() * list.length)]
+}
 
-export const tail = list => list[list.length - 1]
+export function tail<T> (list: T[]): T {
+  return list[list.length - 1]
+}
 
-export const degrees = deg => deg / 180 * PI
+export function degrees (deg: number): number {
+  return deg / 180 * PI
+}
