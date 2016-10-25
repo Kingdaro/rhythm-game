5bfc6a3dc8aad6fcc95e12fd1d775d9c2bed4fa1
Kingdaro <kingdaro@gmail.com>
2016-10-25 11:53:24 -0400

    cleanup

--- src/scoring.ts
+++ src/scoring.ts
@@ -1,27 +1,13 @@
-import * as canvas from './canvas'
-import {clamp, lerp} from './util'
-import {Blue, Orange, Green, Red, Cloudy} from './color'
-
-const {ceil} = Math
-
-export const enum Judgement { None, Break, Good, Perfect, Absolute }
+export const enum Judgement {
+  None,
+  Break,
+  Good,
+  Perfect,
+  Absolute,
+}
 
 export const TimingWindow = {
   Absolute: 15 / 1000,
   Perfect: 70 / 1000,
   Good: 130 / 1000,
-}
-
-const JudgementColor = {
-  [Judgement.Absolute]: Blue,
-  [Judgement.Perfect]: Orange,
-  [Judgement.Good]: Green,
-  [Judgement.Break]: Red,
-}
-
-const JudgementText = {
-  [Judgement.Absolute]: 'ABSOLUTE',
-  [Judgement.Perfect]: 'PERFECT',
-  [Judgement.Good]: 'GOOD',
-  [Judgement.Break]: 'BREAK',
 }
